8166c6e4fcaeb568675cad248faef57e789143b0
Adam Bell <belladam@hawaii.edu>
2024-08-04 12:38:38 -1000

    Rendered WordCloud and Trending Posts graphs

--- SocialMediaNetwork.js
+++ SocialMediaNetwork.js
@@ -1,3 +1,11 @@
+/* Assignment 5 - Social Media Network Diagrams
+ * @author Adam Bell
+ * @author Ben Crawford
+ * @author Tim Lum
+ *
+ * For the graph representations in our html document, we used Vis.js libraries.
+ */
+
 class Comment {
     constructor(username, content, createdAt) {
       this.username = username;
@@ -22,10 +30,6 @@ class Comment {
   
     addView(view) {
       this.views.push(view);
-    }
-  
-    addReference(postId) {
-      this.references.push(postId);
     }
   }
   
@@ -116,9 +120,9 @@ class Comment {
           });
 
           // Highlight important posts based on criteria
-          if (criteria === 'comments' && post.comments.length > 3) {
+          if (criteria === 'comments' && post.comments.length > 2) {
             graph[post.id].important = true;
-          } else if (criteria === 'views' && post.views.length > 4) {
+          } else if (criteria === 'views' && post.views.length > 3) {
             graph[post.id].important = true;
           } else if (criteria === 'blend' && (post.comments.length > 2 || post.views.length > 3)) {
             graph[post.id].important = true;
@@ -136,7 +140,7 @@ class Comment {
       Object.keys(graph).forEach(nodeId => {
         const node = graph[nodeId];
 
-        let nodeOptions = { id: nodeId, label:nodeId };
+        let nodeOptions = { id: nodeId, label: nodeId };
         if (node.type === 'post') {
           nodeOptions.color = node.important ? 'lightcoral' : 'lightgray';
           nodeOptions.shape = 'box';
@@ -158,7 +162,7 @@ class Comment {
       const nodes = new vis.DataSet(nodesArray);
       const edges = new vis.DataSet(edgesArray);
 
-      const container = document.getElementById('graph');
+      const container = document.getElementById('graph1');
       const data = {
         nodes: nodes,
         edges: edges
@@ -166,38 +170,6 @@ class Comment {
       const options = {};
 
       new vis.Network(container, data, options);
-    }
-
-    generateInterestingUsersGraph(criteria) {
-      const graph = {};
-
-      this.users.forEach(user => {
-        graph[user.username] = { type: 'user', connections: [], interesting: false };
-
-        user.connections.forEach(conn => {
-          graph[user.username].connections.push({
-            target: conn.target,
-            type: conn.type
-          });
-        });
-
-        // Determine interesting users based on criteria
-        if (criteria.posts && user.posts.length > criteria.posts) {
-          graph[user.username].interesting = true;
-        }
-        if (criteria.comments && user.comments.length > criteria.comments) {
-          graph[user.username].interesting = true;
-        }
-        if (criteria.attributes) {
-          for (let attr in criteria.attributes) {
-            if (user.attributes[attr] === criteria.attributes[attr]) {
-              graph[user.username].interesting = true;
-            }
-          }
-        }
-      });
-
-      return graph;
     }
 
     generateWordCloud(filters) {
@@ -222,8 +194,33 @@ class Comment {
           });
         });
       });
-
       return wordFrequency;
+    }
+
+    renderWordCloud(wordFrequency) {
+      const nodes = Object.keys(wordFrequency).map((word, index) => {
+        return {
+          id: index + 1,
+          label: word,
+          value: wordFrequency[word],
+          font: {
+            size: wordFrequency[word] * 10
+          }
+        }
+      });
+
+      const data = {
+        nodes: new vis.DataSet(nodes),
+        edges: []
+      };
+
+      const options = {
+        nodes: {
+          shape: 'dot',
+        }
+      }
+      const container = document.getElementById('graph2');
+      new vis.Network(container, data, options);
     }
 
     generateTrendingPostsReport(filters) {
@@ -246,7 +243,7 @@ class Comment {
           const commentRate = post.comments.length / (new Date() - post.createdAt);
           trendingPosts.push({
             post,
-            trendScore: viewRate + commentRate
+            trendScore: (viewRate + commentRate) * 1_000_000
           });
         });
       });
@@ -255,94 +252,43 @@ class Comment {
       return trendingPosts;
     }
 
-    identifyContentClusters() {
-      const clusters = [];
-      const visited = new Set();
-
-      this.users.forEach(user => {
-        user.posts.forEach(post => {
-          if (!visited.has(post.id)) {
-            const cluster = this.dfs(post, visited);
-            clusters.push(cluster);
-          }
-        });
+    renderTrendingPosts(trendingPosts) {
+      const container = document.getElementById("graph3");
+      const textContainer = document.getElementById("graph3Text");
+      textContainer.innerHTML = trendingPosts.map(tp => `<div>${tp.post.id}</div>`).join(' ');
+      const nodes = trendingPosts.map((tp, index) => {
+        return {
+          x: index + 1,
+          y: tp.trendScore,
+          label: { content: tp.post.id }
+        };
       });
-
-      return clusters;
+      const dataSet = new vis.DataSet(nodes);
+      const options = {
+        style: 'bar',
+        barChart: { width: 50, align: 'center' },
+        drawPoints: false,
+        dataAxis: {
+          left: {
+            range: {
+              min: 0,
+            },
+            title: {
+              text: "Trend Score"
+            },
+          },
+        },
+        showMajorLabels: false,
+        showMinorLabels: false,
+        orientation: "top",
+        height: '400px',
+        start: 0,
+        end: 5,
+        clickToUse: true,
+      };
+      new vis.Graph2d(container, nodes, options);
     }
 
-    dfs(post, visited) {
-      const stack = [post];
-      const cluster = [];
-
-      while (stack.length > 0) {
-        const currentPost = stack.pop();
-        if (!visited.has(currentPost.id)) {
-          visited.add(currentPost.id);
-          cluster.push(currentPost);
-
-          currentPost.references.forEach(refId => {
-            const refPost = this.findPost(refId);
-            if (refPost && !visited.has(refPost.id)) {
-              stack.push(refPost);
-            }
-          });
-        }
-      }
-
-      return cluster;
-    }
-
-    identifyUserClusters() {
-      const clusters = [];
-      const visited = new Set();
-
-      this.users.forEach(user => {
-        if (!visited.has(user.username)) {
-          const cluster = this.dfsUsers(user, visited);
-          clusters.push(cluster);
-        }
-      });
-
-      return clusters;
-    }
-
-    dfsUsers(user, visited) {
-      const stack = [user];
-      const cluster = [];
-
-      while (stack.length > 0) {
-        const currentUser = stack.pop();
-        if (!visited.has(currentUser.username)) {
-          visited.add(currentUser.username);
-          cluster.push(currentUser);
-
-          currentUser.connections.forEach(conn => {
-            const connUser = this.findUser(conn.target);
-            if (connUser && !visited.has(connUser.username)) {
-              stack.push(connUser);
-            }
-          });
-        }
-      }
-
-      return cluster;
-    }
-
-    analyzeDirectionalTrends() {
-      const trends = {};
-
-      this.users.forEach(user => {
-        user.connections.forEach(conn => {
-          if (!trends[conn.type]) {
-            trends[conn.type] = 0;
-          }
-          trends[conn.type]++;
-        });
-      });
-
-      return trends;
-    }
   }
   // Example Usage
   
@@ -368,12 +314,12 @@ class Comment {
   user6.addConnection('follows', 'user2');
   
   // Create posts
-  const post1 = new Post('post1', 'This is a post by user1', '2024-07-01T12:00:00Z');
-  const post2 = new Post('post2', 'This is a post by user2', '2024-07-01T12:00:00Z');
-  const post3 = new Post('post3', 'This is a post by user2', '2024-07-01T12:00:00Z');
-  const post4 = new Post('post4', 'This is a post by user4', '2024-07-01T12:00:00Z');
-  const post5 = new Post('post5', 'This is a post by user5', '2024-07-01T12:00:00Z');
-  const post6 = new Post('post6', 'This is a post by user5', '2024-07-01T12:00:00Z');
+  const post1 = new Post('post1', 'This is is is a a post by by by by user1', '2024-08-03T12:00:00Z');
+  const post2 = new Post('post2', 'This is a post by user2', '2024-08-03T12:00:00Z');
+  const post3 = new Post('post3', 'This is a post by user2', '2024-08-03T12:00:00Z');
+  const post4 = new Post('post4', 'This is a post by user4', '2024-08-03T12:00:00Z');
+  const post5 = new Post('post5', 'This is a post by user5', '2024-08-03T12:00:00Z');
+  const post6 = new Post('post6', 'This is a post by user5', '2024-08-03T12:00:00Z');
   
   // Add comments and views to posts
 // Post 1
@@ -472,27 +418,19 @@ user6.addSeenPost('post5', '2024-07-05T11:00:00Z');
 
   
   // Generate graphs and reports
+  // For different importance, pass 'comments' , 'views' , or 'blend'.
   const postUserGraph = network.generatePostUserGraph('blend');
-  const interestingUsersGraph = network.generateInterestingUsersGraph({ posts: 1, comments: 1 });
-  const wordCloud = network.generateWordCloud({ keywords: ['post'], attributes: { location: 'New York' } });
-  const trendingPosts = network.generateTrendingPostsReport({ keywords: ['post'], attributes: { location: 'San Francisco' } });
+  // For different filters, pass keywords[keyword1, keyword2, etc.] as the first argument, and/or pass attributes{location: 'New York', workplace: 'Company A'}
+  const wordCloud = network.generateWordCloud({ keywords: ['post']/*, attributes: { location: 'New York' }*/ });
+  const trendingPosts = network.generateTrendingPostsReport({ keywords: ['post']});
 
   // Rendering Graphs
   network.renderPostUserGraph(postUserGraph);
+  network.renderWordCloud(wordCloud);
+  network.renderTrendingPosts(trendingPosts);
 
-  // Identify clusters of content
-  const contentClusters = network.identifyContentClusters();
-  
-  // Identify clusters of users
-  const userClusters = network.identifyUserClusters();
-  
-  // Analyze directional trends in network usage
-  const directionalTrends = network.analyzeDirectionalTrends();
-  
+  /*
   console.log(postUserGraph);
-  console.log(interestingUsersGraph);
   console.log(wordCloud);
   console.log(trendingPosts);
-  console.log(contentClusters);
-  console.log(userClusters);
-  console.log(directionalTrends);
+*/
